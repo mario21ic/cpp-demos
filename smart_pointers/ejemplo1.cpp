03add00f94a0d6f5b570994b4c44b599bbefce89
Mario Inga <mario21ic@gmail.com>
2020-03-10 23:42:11 -0500

    feat(global) adding smart pointer example

---
+++ smart_pointers/ejemplo1.cpp
@@ -0,0 +1,18 @@
+using namespace std;
+
+void my_func()
+{
+    int* valuePtr = new int(15);
+    int x = 45;
+    // ...
+    if (x == 45)
+        return;   // here we have a memory leak, valuePtr is not deleted
+    // ...
+    delete valuePtr;
+}
+
+int main()
+{
+  my_func();
+  return 0;
+}
