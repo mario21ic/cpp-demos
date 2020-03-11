f4ab110331663be6c4abab7562cce77a2865e0a7
Mario Inga <mario21ic@gmail.com>
2020-03-10 23:41:59 -0500

    feat(global) adding hello example

---
+++ hello/hola_mundo.cpp
@@ -0,0 +1,12 @@
+// hola_mundo.cpp
+// Este programa muestra la palabra "Hola mundo" en la pantalla.
+#include<iostream> // Directiva del procesador.
+ 
+using namespace std;
+// Esta función devuelve un entero.
+// La función del programa empieza aquí.
+int main(){ // Las funciones en C++ empiezan con el signo {.
+    // Sentencia de C++ que muestra en pantalla "Hola Mundo".
+    cout<<"Hola Mundo";
+    return 0; // Esta función devuelve un entero.
+} // Las funciones en C++ finalizan con el signo }.
